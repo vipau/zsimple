871aacdf670c715b0976c3dffd94cf8ddc87ab0c
Vi Pau <696864+vipau@users.noreply.github.com>
2025-11-04 02:30:37 +0100

    use $HOME instead of ~, better quoting

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-datenow=`date +"%Y-%m-%d_%H-%M-%S"`
+datenow="$(date +'%Y-%m-%d_%H-%M-%S')"
 if [ ! -z "$datenow" ]; then
-  cp ~/.zshrc ~/.zshrc.backup.${datenow}
-  cp ~/.aliases ~/.aliases.backup.${datenow}
-  cp ~/.exports ~/.exports.backup.${datenow}
+  cp "$HOME/.zshrc" "$HOME/.zshrc.backup.${datenow}"
+  cp "$HOME/.aliases" "$HOME/.aliases.backup.${datenow}"
+  cp "$HOME/.exports" "$HOME/.exports.backup.${datenow}"
 else
-echo "Couldn't properly name backups with their date! they will be like: ~/.zshrc-zsimplebkp"
-  cp ~/.zshrc ~/.zshrc-zsimplebkp
-  cp ~/.aliases ~/.aliases-zsimplebkp
-  cp ~/.exports /.exports-zsimplebkp
+echo "Couldn't properly name backups with their date! they will be like: $HOME/.zshrc-zsimplebkp"
+  cp "$HOME/.zshrc" "$HOME/.zshrc-zsimplebkp"
+  cp "$HOME/.aliases" "$HOME/.aliases-zsimplebkp"
+  cp "$HOME/.exports" "/.exports-zsimplebkp"
 fi
 
-cp zshrc.zsh ~/.zshrc
-cp aliases.sh ~/.aliases
-cp exports.sh ~/.exports
+cp zshrc.zsh "$HOME/.zshrc"
+cp aliases.sh "$HOME/.aliases"
+cp exports.sh "$HOME/.exports"
