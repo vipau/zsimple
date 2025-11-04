3e5cdf5ecb2a9ae5664ff4e48b36ccb67a461351
Vi Pau <696864+vipau@users.noreply.github.com>
2025-11-04 02:26:39 +0100

    fix redirection to /dev/null

--- exports.sh
+++ exports.sh
@@ -5,10 +5,10 @@ export GPG_TTY=$(tty)
 export HOMEBREW_PREFIX="/opt/homebrew/"
 
 # Set text editor to vim if installed
-command -v vim 2>&1 >/dev/null && export {EDITOR,VISUAL}='vim'
+command -v vim >/dev/null 2>&1  && export {EDITOR,VISUAL}='vim'
 
 # Force less instead of more if installed
-command -v less 2>&1 >/dev/null && export PAGER=less
+command -v less >/dev/null 2>&1 && export PAGER=less
 
 # Add common directories to path
 export PATH="/home/$USER/bin:/home/$USER/bin/*/:/home/$USER/.local/bin:$PATH"
